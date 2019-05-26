17ce91b7b520b051e5a0dbeec9aeb2009ca17c51
Jelena Saric <jelena.saric@fer.hr>
2019-05-26 14:04:49 +0200

    initial project settings added

---
+++ BurgerMaster/src/main/java/hr/fer/zavrsniRad/BurgerMaster/BurgerMasterApplication.java
@@ -0,0 +1,13 @@
+package hr.fer.zavrsniRad.BurgerMaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurgerMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurgerMasterApplication.class, args);
+	}
+
+}
